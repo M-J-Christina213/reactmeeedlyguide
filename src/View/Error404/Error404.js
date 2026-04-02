958b4e3a6cf51e221802c8c0b2de2e8943fd8d45
M-J-Christina213 <m.j.christina213@gmail.com>
2026-04-02 13:49:50 +0530

    installing neccessary packages for routing 🫡

---
+++ src/View/Error404/Error404.js
@@ -0,0 +1,10 @@
+function Error404(){
+    return(
+        <div>
+            <h1> Error 404  page </h1>
+            <p> This is the error 404 page. </p>
+        </div>
+    )
+}
+
+export default Error404;
