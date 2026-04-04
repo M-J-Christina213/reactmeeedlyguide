d1488ae378bb65c1ae67cd32ecca151d730f9fbd
M-J-Christina213 <m.j.christina213@gmail.com>
2026-04-04 21:04:27 +0530

    Refactor Home component to include counter functionality 🫠

--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -1,14 +1,48 @@
 import Navigation from '../Components/Navigation';
 import Footer from '../Components/Footer';
+import React, { useState, useEffect, useRef} from 'react';
+import "../../Style/View/Home/Home.css";
+import { preconnect } from 'react-dom';
+
 function Home(){
-    return(
-        <div>
+    const [count, setCount] = useState(0);
+    const handleIncrease = () => {
+        setCount((prev) => (prev ?? 0) + 1 );
+    }
+
+    const handleDecrease = () => {
+        setCount((prev) => {
+            if((prev ?? 0 ) > 0) return prev - 1;
+            return prev;
+        });
+    };
+
+    //Show loading while count is null
+    if (count === null) return <div>Loading...</div>;
+
+    return (
+        <div className='page-wrapper'>
             <Navigation />
-            <h1> Home page </h1>
-            <p> This is the home page. </p>
-                <Footer />
+            <main className='main-content'>
+                <div className='home-container'>
+                    <h1 className='home-title'> Home Page</h1>
+                    <div className='home-container'> Counter: {count} </div>
+                    <div className='button-group'> 
+                        <button className='button increase' onClick={handleIncrease}> 
+                            Increase 
+                        </button>
+                        <button className='button decrease' onClick={handleDecrease}> 
+                            Decrease 
+                        </button>
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </div>
     )
+
+    
+    
 }
 
 export default Home;
